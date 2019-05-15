22847bb09a2cc701cd1f6b4e99973c2db6b26116
kjy <qa01051@gmail.com>
2019-05-15 17:13:52 +0900

    commit 20190515

--- src/main/java/idu/cs/controller/HomeController.java
+++ src/main/java/idu/cs/controller/HomeController.java
@@ -5,21 +5,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import idu.cs.domain.User;
+import idu.cs.exception.ResourceNotFoundException;
 import idu.cs.repository.UserRepository;
 
 @Controller
 public class HomeController {
 	@Autowired UserRepository userRepo; // Dependency Injection
-	
+	//UserRepository userRepo=new UserRepositoryImpl();
+	//HTTP URI
+	//create=POST
+	//read=GET
+	//update=PUT
+	//delete=DELETE
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("test", "인덕 컴소");
 		model.addAttribute("egy", "유응구");
 		return "index";
+	}
+	@GetMapping("/register")
+	public String getRegForm(Model model) {
+		return "form";
 	}
 	@GetMapping("/users")
 	public String getAllUser(Model model) {
@@ -28,8 +39,19 @@ public class HomeController {
 	}
 	@PostMapping("/users")
 	public String createUser(@Valid @RequestBody User user, Model model) {
+		//user.setName("kjy");
+		//user.setCompany("idu");
 		userRepo.save(user);
 		model.addAttribute("users", userRepo.findAll());
 		return "redirect:/users";
+	}
+	@GetMapping("/users/{id}")
+	public String getUserById(@PathVariable(value = "id") Long userId, Model model)
+			throws ResourceNotFoundException {
+		User user = userRepo.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id :: " + userId));
+		model.addAttribute("name", user.getName());
+		model.addAttribute("company", user.getCompany());
+		return "user";
+		//return ResponseEntity.ok().body(user);
 	}
 }
